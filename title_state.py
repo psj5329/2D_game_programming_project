c549c90a9f248a380dfbbcd9c201499240f9f398
psj5329 <psj5329@naver.com>
2015-11-24 21:37:42 +0900

    code
    start

--- title_state.py
+++ title_state.py
@@ -2,6 +2,7 @@ __author__ = 'SooJeong'
 
 import game_framework
 from pico2d import *
+import main_state
 
 
 name = "TitleState"
@@ -23,9 +24,10 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
                 game_framework.quit()
-        else:
-            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(main_state)
 
 
 def draw():
